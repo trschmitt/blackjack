69c645167231806b24660d3fa909e16fb0755a7e
Tristan Schmitt <trschmitt50@live.com>
2017-08-25 14:46:16 -0400

    Initial commit

---
+++ starter_files/blackjack.rb
@@ -0,0 +1,9 @@
+# The game should start the player with $100 and bets are $10.
+# The only valid moves are hit and stand.
+# Allow the player to keep playing as long as they have money.
+# The dealer uses one deck in their shoe and reshuffles after each round.
+
+puts "Blackjack... The ultimate game of luck and math. Are you Ready? Leh go..."
+
+player pocket = 100
+bet = 10
