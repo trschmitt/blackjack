bb8942612680fd2736f823895bc8edd6d3fb1d16
Tristan Schmitt <trschmitt50@live.com>
2017-09-14 13:56:37 -0400

    Hand_value logic created within game file

--- starter_files/lib/game.rb
+++ starter_files/lib/game.rb
@@ -4,70 +4,40 @@ require_relative "dealer"
 require_relative "user"
 
 class Game
-  
+
   attr_accessor :user, :dealer, :deck
 
   def initialize
     @user = User.new
-    @dealer = Dealer.new
     @deck = Deck.new
+    @deck.shuffle
   end
 
-  def user
-    @user
-  end
+  def hand_value(player)
 
-  def dealer
-    @dealer
-  end
-
-  def deck
-    @deck
-  end
-
-  def deck_shuffle
-    deck.shuffle
-  end
-
-  def user_hand_init
-    2.times do
-      card = deck.draw
-
-    end
-  end
-
-  def user_move
-    while true
-      print "Do you want to (h)it or (s)tand?:"
-      answer = gets.chomp.downcase
-      if answer[1] == "h"
-        user.hand << deck.draw
-        return true
-      elsif answer[1] == "s"
-        return true
+    total = player.hand.reduce(0) do |acc, x|
+      if x.rank.to_s == "J" || x.rank.to_s == "Q" || x.rank.to_s == "K"
+        acc + 10
+      elsif x.rank.to_s == "A"
+        acc + 1
       else
-        return false
+      acc + x.rank.to_i
       end
-      print "Try again brah..."
     end
-  end
 
-  def user_hand_value
-    # if Card.value == 1
-    #   @user.hand_value += 11
-    # elsif Card.value == 2..10
-    #   @user.hand_value += Card.value
-    # end
-  end
+    player.hand.each do |ace|
+      if ace.rank.to_s == "A" && total.to_i <= 10
+        total += 10
+      end
+    end
 
-  def dealer
+    total
 
   end
 
-  def ace_count
-
+  def hit(player)
+    player.hand << deck.draw
   end
-  # official ACE rules The combination of an ace with a card other than a ten-card is known as a "soft hand," because the player can count the ace as a 1 or 11, and either draw cards or not. For example with a "soft 17" (an ace and a 6), the total is 7 or 17. While a count of 17 is a good hand, the player may wish to draw for a higher total. If the draw creates a bust hand by counting the ace as an 11, the player simply counts the ace as a 1 and continues playing by standing or "hitting" (asking the dealer for additional cards, one at a time).
 
   def ran_out_of_cards
     if @deck.cards_length == 0
