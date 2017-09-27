e14667dfd7001949013385418febcb615de1958f
Tristan Schmitt <trschmitt50@live.com>
2017-09-27 10:44:16 -0400

    Finished fixing addition bugs and flow works well

--- starter_files/lib/game.rb
+++ starter_files/lib/game.rb
@@ -2,7 +2,7 @@ require_relative "card"
 require_relative "deck"
 require_relative "dealer"
 require_relative "user"
-require "pry"
+require "byebug"
 
 class Game
 
@@ -28,7 +28,7 @@ class Game
 
     player.hand.each do |card|
       if card.rank.to_s == "A" && total.to_i <= 10
-        total += 10
+        total + 10
       end
     end
 
@@ -37,18 +37,21 @@ class Game
   end
 
   def hit(player)
-    player.hand << @deck.draw
+    player.hand.unshift(@deck.draw)
   end
 
   def dealer_special
     while hand_value(@dealer) < 17
       hit(@dealer)
     end
-    if hand_value(dealer) == 21
+    if hand_value(@dealer) == 21
       print "Dealer's hand is 21... They beat you... Try again mate! \n"
       new_game
+    elsif hand_value(@dealer) > 21
+      print "Dealer busted you win!!\n"
+      new_game
     else
-      print "Dealer's hand is #{hand_value(@dealer)}.\n Your move! \n"
+      print "Dealer's hand is #{hand_value(@dealer)}.\n"
     end
   end
 
@@ -86,6 +89,7 @@ class Game
   end
 
   def play_blackjack
+    # byebug
     @dealer = Dealer.new
 
     while @user.money.to_i > 9
@@ -97,32 +101,37 @@ class Game
       print "This... is Blackjack... Shall we? \n $#{@user.money} is how much you came in with... How much will you leave with? Table bet is $10. \n Your hand is: #{@user.hand[0].rank} of #{@user.hand[0].suit.upcase}, #{@user.hand[1].rank} of #{@user.hand[1].suit.upcase} which means your total hand value is #{hand_value(@user)}\n (H)it or (S)tand?\n"
       bet
 
-      binding.pry
-      
       move = gets.chomp.upcase
-      if move == "H"
-        hit(@user)
-        print "You hit and add #{@user.hand[0].rank} of #{@user.hand[0].suit.upcase} to your hand.\n New total is #{hand_value(@user)}. Do you (H)it or (S)tand?\n"
-        move = gets.chomp.upcase
-      elsif move == "S"
-        print "You stand... Time to see what the Dealer has!\n"
-      else
-        print "Come on... Only two letters to choose from... Which shall it be (Y) or (N)?\n"
-        move = gets.chomp.upcase
+      until hand_value(@user) >= 21 || move == "S"
+        # move = gets.chomp.upcase
+        if move == "H"
+          hit(@user)
+          print "You hit and add #{@user.hand[0].rank} of #{@user.hand[0].suit.upcase} to your hand.\n New total is #{hand_value(@user)}. Do you (H)it or (S)tand?\n"
+          move = gets.chomp.upcase
+        elsif move != "S" || move != "H"
+          print "Come on... Only two letters to choose from... Which shall it be (H) or (S)?\n"
+          move = gets.chomp.upcase
+        else
+          dealer_special
+        end
       end
 
-      if hand_value(@user) > 21
-        print "BUSTED! Better luck next time.\n"
-        new_game
-      elsif hand_value(@user) == 21
-        print "Well would you look at that... You hit 21 you win... Lucky\n"
-        user_wins
-        new_game
-      else
-        dealer_special
-        hand_evaluation
-        new_game
-      end
+        if hand_value(@user) > 21
+          print "BUSTED! Better luck next time.\n"
+          new_game
+        elsif hand_value(@user) == 21
+          print "Well would you look at that... You hit 21 you win... Lucky\n"
+          user_wins
+          new_game
+        else
+          print "Dealer's hand is: #{@dealer.hand[0].rank} of #{@dealer.hand[0].suit.upcase}, #{@dealer.hand[1].rank} of #{@dealer.hand[1].suit.upcase} which means their total hand value is #{hand_value(@dealer)}\n"
+          hand_evaluation
+          new_game
+        end
+
+        if @user.money.to_i < 9
+          print "You don't have any money left mate..."
+        end
     end
   end
 end
